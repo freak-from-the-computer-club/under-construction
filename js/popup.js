9dc937b7e2a1847d064fed3fa33fcedfbefd6f43
fftcc <eee@ff99cc.art>
2022-04-08 21:07:02 +0300

    feat: footer check

---
+++ js/popup.js
@@ -0,0 +1,87 @@
+const popupLinks = document.querySelectorAll('.popup-link');
+const body = document.querySelector('body');
+const lockPadding = document.querySelectorAll('.lock-padding');
+
+let unlock = true;
+
+const timeout = 400;
+
+if (popupLinks.length > 0) {
+  for (let i = 0; i < popupLinks.length; i++) {
+    const popupLinks = popupLinks[i];
+    popupLinks.addEventListener('click', function(e) {
+      const popupName = popupLinks.getAttribute('href').replace('#', '');
+      const currentPopup = document.getElementById(popupName);
+      popupOpen(currentPopup);
+      e.preventDefault();
+    });
+  }
+}
+
+const popupCloseIcon = document.querySelectorAll('.close-popup');
+if (popupCloseIcon.length > 0) {
+  for (let i = 0; i < popupCloseIcon.length; i++) {
+    const el = popupCloseIcon[i];
+    el.addEventListener('click', function(e) {
+      popupClose(el.closest('.popup'));
+      e.preventDefault();
+    });
+  }
+}
+
+function popupOpen(currentPopup) {
+  if (currentPopup && unlock) {
+    const popupActive = document.querySelector('.popup.open');
+    if (popupActive) {
+      popupClose(popupActive, false);
+    } else {
+      bodyLock();
+    }
+    currentPopup.classList.add('open');
+    currentPopup.addEventListener('click', function (e) {
+      if (!e.target.closest('.popup__content')) {
+        popupClose(e.target.closest('.popup'));
+      }
+    });
+  }
+}
+
+function popupClose(popupActive, doUnlock = true) {
+  if (unlock) {
+    popupActive.classList.remove('open');
+    if (doUnlock) {
+      bodyUnLock();
+    }
+    currentPopup.classList.add('open');
+    currentPopup.addEventListener('click', function (e) {
+      if (!e.target.closest('.popup__content')) {
+        popupClose(e.target.closest('.popup'));
+      }
+    });
+  }
+}
+
+function bodyLock() {
+  const lockPaddingValue = window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px';
+
+  for (let i =0; i < lockPadding.length; i++) {
+    const el = lockPadding[i];
+    el.style.paddingRight = lockPaddingValue;
+  }
+  body.style.paddingRight = lockPaddingValue;
+  body.classList.add('lock');
+
+  unlock = false;
+  setTimeout(function () {
+    unlock = true;
+  }, timeout);
+}
+
+function bodyUnLock() {
+  setTimeout(function () {
+    for (let i = 0; i < lockPadding.length; i++) {
+      const el = lockPadding[i];
+      el.style.paddingRight = '0px';
+    }
+  })
+}
