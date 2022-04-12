7da974d0c0dd9f7c10d800682bc58f59c3d02caa
fftcc <eee@ff99cc.art>
2022-04-12 20:33:46 +0300

    fix: footer/text adaptive

--- js/currentVersion.js
+++ js/currentVersion.js
@@ -1,6 +1,6 @@
 function currentVersion() {
   const verEl = document.querySelectorAll("#current-version");
-  const verName = '0.1.4-jean-michel-basquiat--milky-pink';
+  const verName = '0.1.5-jean-michel-basquiat--cream-pink';
   for(let i = 0; i < verEl.length; i++) {
     verEl[i].textContent = verName;
   }
