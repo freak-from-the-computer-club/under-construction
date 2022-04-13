7db92afabc085e39923fac03a4bbbe832e0bc83a
fftcc <eee@ff99cc.art>
2022-04-13 18:27:35 +0300

    ref: refactoring and full webpack configuration

--- src/components/currentVersion.js
+++ src/components/currentVersion.js
@@ -1,6 +1,6 @@
 function currentVersion() {
   const verEl = document.querySelectorAll("#current-version");
-  const verName = '0.1.5-jean-michel-basquiat--cream-pink';
+  const verName = '0.1.6-jean-michel-basquiat--igor-pink';
   for(let i = 0; i < verEl.length; i++) {
     verEl[i].textContent = verName;
   }
